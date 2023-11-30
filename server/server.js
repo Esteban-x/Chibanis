e9a4a2e44187befca16d48f8b2ce20eb7f3cd247
Tiagzo <130380375+Tiagzo@users.noreply.github.com>
2023-11-30 19:33:19 +0100

    back inscription

--- server/server.js
+++ server/server.js
@@ -1,28 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const UserModel = require('./models/Users')
 
 const app = express()
+app.use(cors());
+app.use(express.json())
 
-app.use(bodyParser.json())
+mongoose.connect('mongodb://127.0.0.1:27017/chibanis')
+
+app.post('/register', (req, res)=>{
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
 
 app.listen(3000, () => console.log('Le serveur est lancé sur le port 3000'))
-
-mongoose.connect('mongodb://localhost:27017/Chibanis', {useNewUrlParser: true, useUnifiedTopology: true})
-
-const User = mongoose.model('User', new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number,
-    role: String,
-    isVulnerable: Boolean,
-    password: String,
-}))
-
-app.post('/register', async(req, res)=>{
-    const { name, email, password, age, isVulnerable, role } = req.body
-    const user = new User({name, password, isVulnerable, email, age, role})
-    await user.save()
-    res.send(user)
-})
 
